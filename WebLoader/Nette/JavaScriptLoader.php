f897307a795cd724f884ebea766083d62f1a7801
vojtech_kohout <v.kohout@gmail.com>
2014-04-30 13:31:10 +0200

    Added data attributes based variables support

--- WebLoader/Nette/JavaScriptLoader.php
+++ WebLoader/Nette/JavaScriptLoader.php
@@ -3,6 +3,7 @@
 namespace WebLoader\Nette;
 
 use Nette\Utils\Html;
+use WebLoader\InvalidArgumentException;
 
 /**
  * JavaScript loader
@@ -13,6 +14,22 @@ use Nette\Utils\Html;
 class JavaScriptLoader extends WebLoader
 {
 
+	/** @var array */
+	private $dataAttributes = array();
+
+	/**
+	 * @param string $name
+	 * @param mixed $value
+	 * @throws \WebLoader\InvalidArgumentException
+	 */
+	public function setVariable($name, $value)
+	{
+		if (!preg_match('#^[-a-zA-Z0-9_:.]+$#', $name)) {
+			throw new InvalidArgumentException("Invalid variable name given: $name");
+		}
+		$this->dataAttributes['data-' . $name] = $value;
+	}
+
 	/**
 	 * Get script element
 	 * @param string $source
@@ -20,7 +37,12 @@ class JavaScriptLoader extends WebLoader
 	 */
 	public function getElement($source)
 	{
-		return Html::el("script")->type("text/javascript")->src($source);
+		$el = Html::el("script")->type("text/javascript")->src($source);
+		if (!empty($this->dataAttributes)) {
+			$el->class = 'jsLoader';
+			$el->addAttributes($this->dataAttributes);
+		}
+		return $el;
 	}
 
 }
